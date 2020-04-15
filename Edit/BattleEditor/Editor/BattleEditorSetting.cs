2407e820a6727755103ed948cd37a57f64d03220
a26053200 <zhengnan262@qq.com>
2020-04-15 23:54:37 +0800

    battle editor

---
+++ Edit/BattleEditor/Editor/BattleEditorSetting.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BattleEditor
+{
+    public class BattleEditorSetting : ScriptableObject
+    {
+        
+    }
+}
---
+++ Edit/BattleEditor/Editor/GUI/BattleEditorWnd.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace BattleEditor
+{
+    public class BattleEditorWnd : EditorWindow
+    {
+        [MenuItem("BattleEditor/Open")]
+        static void ShowWnd()
+        {
+            Rect rect = new Rect(100,100,1024,768);
+            BattleEditorWnd wnd = EditorWindow.GetWindow<BattleEditorWnd>(true, "战斗编辑器");
+            wnd.position = rect;
+            wnd.Init();
+        }
+
+        private const string SettingPath = "Assets/Edit/BattleEditorSetting.asset";
+        private BattleEditorSetting _setting;
+        private void Init()
+        {
+            if (!File.Exists(SettingPath))
+                EditUtility.CreateAsset<BattleEditorSetting>(SettingPath);
+            _setting = AssetDatabase.LoadAssetAtPath<BattleEditorSetting>(SettingPath);
+        }
+    }
+}
