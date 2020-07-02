0916f1f45f2cb1bd87c1d14689c52bad983c02cd
a26053200 <zhengnan262@qq.com>
2020-07-03 00:15:04 +0800

    battle editor

--- Lua/Config/ViewConfig.lua
+++ Lua/Config/ViewConfig.lua
@@ -1,29 +1,29 @@
 ViewConfig = {}
-ViewConfig.World = {module = "World",name = "World",prefab = "Prefabs/UI/Common/World.prefab",}
-ViewConfig.RoleInfo = {module = "Role",name = "RoleInfo",prefab = "Prefabs/UI/Role/RoleInfo.prefab",}
-ViewConfig.ServerList = {module = "Login",name = "ServerList",prefab = "Prefabs/UI/Login/ServerListWnd.prefab",}
-ViewConfig.RoleCreate = {module = "Login",name = "RoleCreate",prefab = "Prefabs/UI/Login/RoleCreate.prefab",}
-ViewConfig.RoleSelect = {module = "Login",name = "RoleSelect",prefab = "Prefabs/UI/Login/RoleSelectWnd.prefab",}
-ViewConfig.Notice = {module = "Login",name = "Notice",prefab = "Prefabs/UI/Login/NoticeWnd.prefab",}
-ViewConfig.LoginScene = {module = "Login",name = "LoginScene",prefab = "Prefabs/UI/Login/LoginScene.prefab",}
-ViewConfig.Login = {module = "Login",name = "Login",prefab = "Prefabs/UI/Login/LoginWnd.prefab",}
-ViewConfig.Loading = {module = "Loading",name = "Loading",prefab = "Prefabs/UI/Common/LoadingWnd.prefab",}
-ViewConfig.Joystick = {module = "Joystick",name = "Joystick",prefab = "Prefabs/UI/Common/Joystick.prefab",}
-ViewConfig.Battle = {module = "Battle",name = "Battle",prefab = "Prefabs/UI/Battle/Battle.prefab",}
-ViewConfig.BattleInfo = {module = "Battle",name = "BattleInfo",prefab = "Prefabs/UI/Battle/BattleInfo.prefab",}
-ViewConfig.Navigation = {name = "Navigation",module = "Lobby",prefab = "Prefabs/UI/Lobby/Navigation.prefab",}
-ViewConfig.BattleEditor = {name = "BattleEditor",module = "Battle",prefab = "Prefabs/UI/Battle/BattleEditor.prefab",}
-ViewConfig.ResourceBar = {name = "ResourceBar",module = "Role",prefab = "Prefabs/UI/Common/ResourceBar.prefab",}
-ViewConfig.PlayerInfo = {name = "PlayerInfo",module = "Player",}
-ViewConfig.CardDraw = {name = "CardDraw",module = "Card",prefab = "Prefabs/UI/Card/CardDraw.prefab",}
-ViewConfig.Lobby = {name = "Lobby",module = "Lobby",prefab = "Prefabs/UI/Lobby/Lobby.prefab",}
-ViewConfig.CardList = {name = "CardList",module = "Card",prefab = "Prefabs/UI/Card/CardList.prefab",}
-ViewConfig.DrawCardList = {name = "DrawCardList",module = "Card",prefab = "Prefabs/UI/Card/DrawCardList.prefab",}
 ViewConfig.Adventure = {name = "Adventure",module = "Adventure",prefab = "Prefabs/UI/Adventure/Adventure.prefab",}
-ViewConfig.ItemBag = {name = "ItemBag",module = "Item",}
+ViewConfig.Battle = {module = "Battle",name = "Battle",prefab = "Prefabs/UI/Battle/Battle.prefab",}
 ViewConfig.BattleConfig = {module = "BattleConfig",name = "BattleConfig",prefab = "Prefabs/UI/BattleConfig/BattleConfig.prefab",}
+ViewConfig.BattleEditor = {name = "BattleEditor",module = "Battle",prefab = "Prefabs/UI/Battle/BattleEditor.prefab",}
+ViewConfig.BattleInfo = {module = "Battle",name = "BattleInfo",prefab = "Prefabs/UI/Battle/BattleInfo.prefab",}
+ViewConfig.BattleResult = {module = "Battle",name = "BattleResult",prefab = "Prefabs/UI/Battle/BattleResult.prefab",}
+ViewConfig.CardDraw = {name = "CardDraw",module = "Card",prefab = "Prefabs/UI/Card/CardDraw.prefab",}
+ViewConfig.CardList = {name = "CardList",module = "Card",prefab = "Prefabs/UI/Card/CardList.prefab",}
 ViewConfig.CheckPoint = {name = "CheckPoint",module = "CheckPoint",prefab = "Prefabs/UI/CheckPoint/CheckPoint.prefab",}
 ViewConfig.CheckPointInfo = {name = "CheckPointInfo",module = "CheckPoint",prefab = "Prefabs/UI/CheckPoint/CheckPointInfo.prefab",}
-ViewConfig.Transition = {name = "Transition",module = "Transition",prefab = "Prefabs/UI/Common/Transition.prefab",}
+ViewConfig.DrawCardList = {name = "DrawCardList",module = "Card",prefab = "Prefabs/UI/Card/DrawCardList.prefab",}
+ViewConfig.ItemBag = {name = "ItemBag",module = "Item",}
+ViewConfig.Joystick = {module = "Joystick",name = "Joystick",prefab = "Prefabs/UI/Common/Joystick.prefab",}
+ViewConfig.Loading = {module = "Loading",name = "Loading",prefab = "Prefabs/UI/Common/LoadingWnd.prefab",}
+ViewConfig.Lobby = {name = "Lobby",module = "Lobby",prefab = "Prefabs/UI/Lobby/Lobby.prefab",}
+ViewConfig.Login = {module = "Login",name = "Login",prefab = "Prefabs/UI/Login/LoginWnd.prefab",}
+ViewConfig.LoginScene = {module = "Login",name = "LoginScene",prefab = "Prefabs/UI/Login/LoginScene.prefab",}
+ViewConfig.Navigation = {name = "Navigation",module = "Lobby",prefab = "Prefabs/UI/Lobby/Navigation.prefab",}
+ViewConfig.Notice = {module = "Login",name = "Notice",prefab = "Prefabs/UI/Login/NoticeWnd.prefab",}
+ViewConfig.PlayerInfo = {name = "PlayerInfo",module = "Player",}
 ViewConfig.PveBattleInfo = {name = "PveBattleInfo",module = "Battle",prefab = "Prefabs/UI/Battle/PveBattleInfo.prefab",}
-ViewConfig.BattleResult = {module = "Battle",name = "BattleResult",prefab = "Prefabs/UI/Battle/BattleResult.prefab",}
+ViewConfig.ResourceBar = {name = "ResourceBar",module = "Role",prefab = "Prefabs/UI/Common/ResourceBar.prefab",}
+ViewConfig.RoleCreate = {module = "Login",name = "RoleCreate",prefab = "Prefabs/UI/Login/RoleCreate.prefab",}
+ViewConfig.RoleInfo = {module = "Role",name = "RoleInfo",prefab = "Prefabs/UI/Role/RoleInfo.prefab",}
+ViewConfig.RoleSelect = {module = "Login",name = "RoleSelect",prefab = "Prefabs/UI/Login/RoleSelectWnd.prefab",}
+ViewConfig.ServerList = {module = "Login",name = "ServerList",prefab = "Prefabs/UI/Login/ServerListWnd.prefab",}
+ViewConfig.Transition = {name = "Transition",module = "Transition",prefab = "Prefabs/UI/Common/Transition.prefab",}
+ViewConfig.World = {module = "World",name = "World",prefab = "Prefabs/UI/Common/World.prefab",}
